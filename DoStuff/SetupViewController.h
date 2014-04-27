325569bf954080ab8dae1ad98470269ff008b583
ollelind <mail@ollelind.se>
2014-04-28 01:41:25 +0200

    Hello world

---
+++ DoStuff/SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  DoStuff
+//
+//  Created by Olle Lind on 28/04/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
---
+++ DoStuff/SetupViewController.m
@@ -0,0 +1,38 @@
+//
+//  SetupViewController.m
+//  DoStuff
+//
+//  Created by Olle Lind on 28/04/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import "SetupViewController.h"
+
+@interface SetupViewController ()
+
+@end
+
+@implementation SetupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
