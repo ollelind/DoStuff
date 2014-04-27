c2f75373e419bfebcde6f2f7c9243bb71c23a032
ollelind <mail@ollelind.se>
2014-04-28 01:38:32 +0200

    Initial Commit

---
+++ DoStuff/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DoStuff
+//
+//  Created by Olle Lind on 28/04/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
