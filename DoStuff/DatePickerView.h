2be613e7007ac05b7a4870840abf452af7f20c07
ollelind <mail@ollelind.se>
2014-05-11 01:13:13 +0200

    Added view for create activity date picker

---
+++ DoStuff/DatePickerView.h
@@ -0,0 +1,14 @@
+//
+//  DatePickerView.h
+//  DoStuff
+//
+//  Created by Olle Lind on 10/05/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DatePickerView : UIView
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ DoStuff/DatePickerView.m
@@ -0,0 +1,26 @@
+//
+//  DatePickerView.m
+//  DoStuff
+//
+//  Created by Olle Lind on 10/05/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import "DatePickerView.h"
+
+@implementation DatePickerView
+
+-(void)awakeFromNib{
+    self.scrollView.backgroundColor = [UIColor whiteColor];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
