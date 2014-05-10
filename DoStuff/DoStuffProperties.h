1d6b5f411d72fbbf8847372fca575061c9446182
ollelind <mail@ollelind.se>
2014-05-10 18:02:58 +0200

    Added global properties

---
+++ DoStuff/DoStuffProperties.h
@@ -0,0 +1,19 @@
+//
+//  DoStuffProperties.h
+//  DoStuff
+//
+//  Created by Olle Lind on 28/04/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#ifndef DoStuff_DoStuffProperties_h
+#define DoStuff_DoStuffProperties_h
+
+
+#pragma mark - Signals
+#define SETUP_FINISHED @"SETUP_FINISHED"
+
+// Custom colors
+#define COLOR_BACKGROUND_GREY [UIColor colorWithRed:240/255.0f green:240/255.0f blue:240/255.0f alpha:1.0f]
+#define COLOR_ORANGE [UIColor colorWithRed:255/255.0f green:210/255.0f blue:88/255.0f alpha:1.0f]
+#endif
---
+++ DoStuff/Helpers.h
@@ -0,0 +1,15 @@
+//
+//  Helpers.h
+//  DoStuff
+//
+//  Created by Olle Lind on 02/05/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Helpers : NSObject
+
++(NSString *)stringFromValue:(id)value;
+
+@end
