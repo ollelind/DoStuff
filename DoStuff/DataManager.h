1b1015ea0dc0c609151c9522b7006d50d12613e0
ollelind <mail@ollelind.se>
2014-05-10 18:00:02 +0200

    Refactored Core data stuff into DataManager

---
+++ DoStuff/DataManager.h
@@ -0,0 +1,21 @@
+//
+//  DataManager.h
+//  DoStuff
+//
+//  Created by Olle Lind on 02/05/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface DataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++(DataManager *)client;
+-(void)saveContext;
+
+@end
