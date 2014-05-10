595d98ffa01af4585951758ad79e03b3a5fab4d4
ollelind <mail@ollelind.se>
2014-05-10 17:59:26 +0200

    Added DataFactory

---
+++ DoStuff/DataFactory.h
@@ -0,0 +1,16 @@
+//
+//  DataFactory.h
+//  DoStuff
+//
+//  Created by Olle Lind on 07/05/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataFactory : NSObject
+
+-(void)populateCategories;
+-(void)populateActivities;
+
+@end
---
+++ DoStuff/DataFactory.m
@@ -0,0 +1,47 @@
+//
+//  DataFactory.m
+//  DoStuff
+//
+//  Created by Olle Lind on 07/05/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import "DataFactory.h"
+#import "ActivityCategoryDAO.h"
+#import "Activity.h"
+#import "ActivityDAO.h"
+#import "User.h"
+#import "UserDAO.h"
+
+@implementation DataFactory
+
+-(void)populateCategories{
+    ActivityCategoryDAO *dao = [ActivityCategoryDAO buildDAO];
+    [dao newItemWithName:@"Drinking" imageURL:@"olle_profile"];
+    [dao newItemWithName:@"Sporting" imageURL:@"olle_profile"];
+    [dao newItemWithName:@"Gaming" imageURL:@"olle_profile"];
+}
+
+-(void)populateActivities{
+    UserDAO *userDao = [UserDAO buildDAO];
+    NSArray *users = [userDao listEntities];
+    if(users.count > 0){
+        ActivityCategoryDAO *acDao = [ActivityCategoryDAO buildDAO];
+        NSArray *categories = [acDao listEntities];
+        ActivityDAO *activityDao = [ActivityDAO buildDAO];
+        if(categories.count > 0){
+            for(int i=0; i<20; i++){
+                User *user = users[arc4random()%users.count];
+                ActivityCategory *category = categories[arc4random()%categories.count];
+                [activityDao newActivityWithIdentifier:arc4random()%100000
+                                                hostID:user.identifier
+                                                  name:category.name
+                                           information:@"Information about the activity, place etc.."
+                                    activityCategories:@[category]
+                                                  date:[NSDate dateWithTimeIntervalSinceNow:-arc4random()%60*60*24*10]];
+            }
+        }
+    }
+}
+
+@end
