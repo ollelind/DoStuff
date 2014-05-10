62861125ec3586228544641ad44cd989f8f6b7a2
ollelind <mail@ollelind.se>
2014-05-10 18:00:48 +0200

    Added Core data stuff

--- DoStuff/AppDelegate.h
+++ DoStuff/AppDelegate.h
@@ -7,16 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CustomTabBarController.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
+@property (nonatomic, strong) CustomTabBarController *tabBarController;
 
 @end
---
+++ DoStuff/AppDelegate.m
@@ -0,0 +1,108 @@
+//
+//  AppDelegate.m
+//  DoStuff
+//
+//  Created by Olle Lind on 28/04/14.
+//  Copyright (c) 2014 Olle Lind. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "SetupViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
+#import "FacebookHandler.h"
+#import "FriendsListViewController.h"
+#import "DataManager.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    
+    // Init clients
+    [DataManager client];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setupFinished) name:SETUP_FINISHED object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fbLoggedIn) name:FB_LOGGED_IN object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fbLoggedOut) name:FB_LOGGED_OUT object:nil];
+    
+    [FBLoginView class];
+
+    [[FacebookHandler client] checkIfFacebookIsAuthenticated];
+    
+    return YES;
+}
+
+-(void)fbLoggedIn{
+    [self showTabBar];
+}
+-(void)fbLoggedOut{
+    [self showSetup];
+}
+
+-(void)showSetup{
+    SetupViewController *setupViewController = [[SetupViewController alloc]init];
+    [self.window setRootViewController:setupViewController];
+}
+-(void)showTabBar{
+    self.tabBarController = [[CustomTabBarController alloc]initAnimated:YES];
+    [self.window setRootViewController:self.tabBarController];
+}
+
+-(void)setupFinished{
+    [self showTabBar];
+}
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    [FBSession.activeSession setStateChangeHandler:
+     ^(FBSession *session, FBSessionState state, NSError *error) {
+         // Call the sessionStateChanged:state:error method to handle session state changes
+         [[FacebookHandler client] sessionStateChanged:session state:state error:error];
+     }];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    // Handle the user leaving the app while the Facebook login dialog is being shown
+    // For example: when the user presses the iOS "home" button while the login dialog is active
+    [FBAppCall handleDidBecomeActive];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Saves changes in the application's managed object context before the application terminates.
+    [[DataManager client] saveContext];
+}
+
+@end
