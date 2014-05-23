402cd4fe00fe7991e77143bfc046832d6f1bf2aa
ollelind <mail@ollelind.se>
2014-05-23 23:51:21 +0200

    Changed main color

--- DoStuff/DoStuffProperties.h
+++ DoStuff/DoStuffProperties.h
@@ -13,9 +13,13 @@
 #pragma mark - Signals
 #define SETUP_FINISHED @"SETUP_FINISHED"
 
+// Custom UI
+static NSString *fontName = @"Roboto";
+
 // Custom colors
 #define COLOR_BACKGROUND_GREY [UIColor colorWithRed:240/255.0f green:240/255.0f blue:240/255.0f alpha:1.0f]
 #define COLOR_ORANGE [UIColor colorWithRed:255/255.0f green:210/255.0f blue:88/255.0f alpha:1.0f]
 #define COLOR_DARK_GREY [UIColor colorWithRed:60/255.0f green:60/255.0f blue:60/255.0f alpha:1.0f]
 #define COLOR_LIGHT_GREY [UIColor colorWithRed:230/255.0f green:230/255.0f blue:230/255.0f alpha:1.0f]
+#define COLOR_MAIN [UIColor colorWithRed:0/255.0f green:204/255.0f blue:255/255.0f alpha:1.0f]
 #endif
